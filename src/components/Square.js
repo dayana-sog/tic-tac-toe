49ffce5717b5e0d58cb0fa17c0cff4d190f32b79
dayana-sog <dayanasog23@gmail.com>
2020-09-21 20:37:41 +0100

    correções image Square

--- src/components/Square.js
+++ src/components/Square.js
@@ -6,11 +6,13 @@ import imgX from '../assets/X.ico';
 const Square = ({ value, onClick }) => {
 
   if (value === 'X') {
-    return <img src={imgX} alt="X" style={{ width: '150px', height: '150px'}} />
+    return <img src={imgX} alt="X" style={{ width: '150px', height: '150px', alignSelf: 'center',
+    justifySelf: 'center'}} />
   } 
 
   if (value === 'O') {
-    return <img src={imgO} alt="O" style={{ width: '150px', height: '150px' }} />
+    return <img src={imgO} alt="O" style={{ width: '150px', height: '150px', alignSelf: 'center',
+    justifySelf: 'center' }} />
   }
 
   return (
